7298d7cada2ef4db9146ccacc6620b52b6af817d
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-02-19 02:24:25 +0100

    gzcmd.sh: about installation and md5sum check, v0.1.3
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- gzcmd.sh
+++ gzcmd.sh
@@ -6,6 +6,7 @@
 # Usage   : gzcmd.sh /path/elf-executable[.gz] [filename] [blocksize]
 # Hint    : set blocksize as headersize +2 from gzcmd.sh for min.size
 # Host    : [[export] GZTMPDIR=path GZUNGZIP=pigz;] [shell] elf.gz.sh
+# Install : sudo sh -c "[export] GZTMPDIR=/usr/local/bin; elf.gz.sh"
 #
 # Suggestion for minimal size with musl static compilation of a single file.c:
 #
@@ -31,6 +32,11 @@
 # executable to compress, including scripts on which UPX fails, obviously.
 #
 # Feedback from UPX: github.com/upx/upx/issues/911#issuecomment-3922221407
+#
+# Using a storage temporary path instead of a memory one, increases a bit
+# the 1st call but speed-up all the others, especially for large archives.
+# By extension, root can use it for a temporary or permanent installation.
+# Instead the remote installation wget $url -O- | sh is still missing by $0.
 #
 ################################################################################
 if [ "x${1:-}" = "x--do-tests" ]; then #########################################
@@ -183,7 +189,7 @@ MD5CKSUM=$(md5sum "$gzelf"  | cut -d' ' -f1)
 gzelfle="$ORIGNAME.gz.sh"
 BLKSIZE=${3:-32}
 ZCMPLVL=${4:-9}
-
+# md5sum check after gunzip was for debug only, a corrupted archive fails anyway.
 headstr=$(cat <<EOF
 #!/bin/sh
 # (c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, MIT license
@@ -195,7 +201,7 @@ MD5="$MD5CKSUM"
 exec 3>/dev/null
 uid=\$(id -u || echo 1000)
 test -n "\$PATH" || export PATH=/bin:/usr/bin:/usr/local/bin
-if [ !  -r "\$0" ]; then echo "ERROR: '\$0' is not readable" >&2; exit 1; fi
+test -r "\$0" || { echo "ERROR: '\$0' is not readable (\$@)" >&2; exit 1; }
 mdc() { [ -r "\$fn" ] && md5sum "\$fn" | grep -qe "^\$MD5 "; } 
 gpm() { grep -qe "\$@" /proc/mounts; }
 
@@ -210,12 +216,12 @@ if ! mdc; then
   for i in \${GZUNGZIP:-} pigz gzip zcat; do
     uz=\$i; which \$uz >&3 && break
   done
-  wn="\$fn.\$(date +%N)"; gpm "tmpfs.*/dev/shm" &&
+  wn="\$fn.\$(date +%N)"; gpm "tmpfs.*\$tmp" &&
     trap 'rm -f "\$fn" "\$wn"; rmdir "\$dn" 2>&3' EXIT INT TERM
   ( umask 007; mkdir -p "\$dn" && touch "\$wn" &&
     chmod 0700 "\$dn" "\$wn" ) || exit 1
   dd if=\$0 skip=1 bs=HDRSIZE status=none | \$uz -dc >"\$wn" &&
-    mv -f "\$wn" "\$fn" && mdc || exit 1
+    mv -f "\$wn" "\$fn"        || exit 1
 fi
 
 eval sh -c "'\$fn \$@'"
