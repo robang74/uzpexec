eca212d03d36c4bfbc760a0400e4f5d0202b9889
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-06-28 01:17:09 +0200

    uzpack.sh: use printf instead of echo \n, bugfix
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- uzpack.sh
+++ uzpack.sh
@@ -122,13 +122,13 @@ while true; do
 
     # Safety check about the destination filename to avoid argv[0] underflow
     bdst=$(basename "$dst")
-    if   echo "$bdst" | grep -qe '^.$' -e '^..$'; then
+    if   printf "%s" "$bdst" | grep -qe '^.$'  -e '^..$'; then
         echo "Error: destination filename '$bdst' too short, min 3"
         break
     fi
-    if   echo "$bdst" | grep -qe     ".\{4\}xec$"; then
+    if   printf "%s" "$bdst" | grep -qe     ".\{4\}xec$"; then
         true
-    elif echo "$bdst" | grep -qe "uzp$" -e "xec$"; then
+    elif printf "%s" "$bdst" | grep -qe "uzp$" -e "xec$"; then
         echo "Error: destination filename '$bdst' too short, min 7"
         break
     fi
