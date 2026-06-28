0317a506443199e449bbcef377398d1bfc1d8fc1
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-06-28 03:49:38 +0200

    sigsegv.c: should prints warnings when compiled
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- sigsegv.c
+++ sigsegv.c
@@ -1,6 +1,9 @@
 // sigsegv.c — compila: cc -s -Os sigsegv.c -o sigsegv
 #include <unistd.h>
 int main(int argc, char *argv[]) {
-    execve(argv[1], (char *[]){NULL}, (char *[]){NULL});
+#pragma message "!!!"
+#pragma message "Following two warnings are expected here"
+#pragma message "!!!"
+    execve(argv[1], NULL, (char *[]){NULL});
     return 127;
 }
